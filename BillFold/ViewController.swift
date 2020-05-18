24d07a74797eb07e4779c5b0dfc0c821d480ed8d
Simon Schueller <simonschueller@Simons-MBP.lan>
2020-05-18 14:58:17 -0400

    Make screen scroll with up with keyboard

--- BillFold/ViewController.swift
+++ BillFold/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK: -Outlets
     @IBOutlet weak var moneyTextField: UITextField!
     @IBOutlet weak var moneyLabel: UILabel!
     
+    //MARK: -Variables
     var amt: Int = 0
     var total: Double = 0.00
     
@@ -23,35 +25,29 @@ class ViewController: UIViewController, UITextFieldDelegate {
         moneyTextField.placeholder = updateAmount()
         moneyTextField.keyboardType = UIKeyboardType.numberPad
         moneyLabel.text = "$0.00"
+        //Listen for Keyboard events
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
-    @IBAction func resetBalance(_ sender: UIButton) {
-        total = 0
-        moneyLabel.text = formatMoneyLabel(total)
+    deinit {
+        //Stop listening for keyboard events
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
-    @IBAction func addMoney(_ sender: UIButton) {
-        if let money = moneyTextField.text{
-            let index = money.index(money.startIndex, offsetBy: 1)
-            if let currentMoney = Double(money.suffix(from: index)){
-                total += currentMoney
-            }
-            moneyLabel.text = formatMoneyLabel(total)
+    //MARK: -Methods or Functions
+    @objc func keyboardWillChange(notification: Notification){
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else{
+            return
         }
-        moneyTextField.text = ""
-        amt = 0
-    }
-    
-    @IBAction func subtractMoney(_ sender: UIButton) {
-        if let money = moneyTextField.text{
-            let index = money.index(money.startIndex, offsetBy: 1)
-            if let currentMoney = Double(money.suffix(from: index)){
-                total -= currentMoney
-            }
-            moneyLabel.text = formatMoneyLabel(total)
+        if notification.name == UIResponder.keyboardWillShowNotification || notification.name == UIResponder.keyboardWillChangeFrameNotification {
+            view.frame.origin.y = -keyboardRect.height
+        }else{
+            view.frame.origin.y = 0
         }
-        moneyTextField.text = ""
-        amt = 0
     }
     
     func textField(_ textfield: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
@@ -83,5 +79,38 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    //MARK: -Actions
+    @IBAction func resetBalance(_ sender: UIButton) {
+        total = 0
+        moneyLabel.text = formatMoneyLabel(total)
+    }
+    
+    @IBAction func addMoney(_ sender: UIButton) {
+        self.view.endEditing(true)
+        if let money = moneyTextField.text{
+            let index = money.index(money.startIndex, offsetBy: 1)
+            if let currentMoney = Double(money.suffix(from: index)){
+                total += currentMoney
+            }
+            moneyLabel.text = formatMoneyLabel(total)
+        }
+        moneyTextField.text = ""
+        amt = 0
+    }
+    
+    @IBAction func subtractMoney(_ sender: UIButton) {
+        self.view.endEditing(true)
+        if let money = moneyTextField.text{
+            let index = money.index(money.startIndex, offsetBy: 1)
+            if let currentMoney = Double(money.suffix(from: index)){
+                total -= currentMoney
+            }
+            moneyLabel.text = formatMoneyLabel(total)
+        }
+        moneyTextField.text = ""
+        amt = 0
+    }
+    
 }
 
